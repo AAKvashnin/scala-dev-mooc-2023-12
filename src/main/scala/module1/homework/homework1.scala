76aacc98af0b0e4c44711402e6d86441c99d4c60
AAKvashnin <alexey.kvashnin@gmail.com>
2024-05-26 18:20:58 +0300

    Added homework1

--- src/main/scala/module1/homework/homework1.scala
+++ src/main/scala/module1/homework/homework1.scala
@@ -2,20 +2,24 @@ package module1.homework
 
 
 import scala.util.Random
-/*
+
 class BallsExperiment {
 
+  val balls:List[Boolean]=List(true,true,true,false,false,false)
+
   def isFirstBlackSecondWhite(): Boolean = {
-    ???
+    val first=Random.between(0,6)
+    val second=Random.between(0,5)
+    !balls.apply(first) && balls.zipWithIndex.filter(_._2 != first).map(_._1).apply(second)
   }
 }
 
 object BallsTest {
   def main(args: Array[String]): Unit = {
     val count = 10000
-    val listOfExperiments: List[BallsExperiment] = ???
-    val countOfExperiments = ???
+    val listOfExperiments: List[BallsExperiment] = List.fill(count)(new BallsExperiment)
+    val countOfExperiments = listOfExperiments.map(_.isFirstBlackSecondWhite())
     val countOfPositiveExperiments: Float = countOfExperiments.count(_ == true)
     println(countOfPositiveExperiments / count)
   }
-}*/
+}
