174c966cc6cfb0c202b8f4c560ab66d34c8f7d2e
AAKvashnin <alexey.kvashnin@gmail.com>
2024-06-08 23:09:06 +0300

    Added homework4

--- src/main/scala/module1/futures/task_futures_sequence.scala
+++ src/main/scala/module1/futures/task_futures_sequence.scala
@@ -26,7 +26,7 @@ object task_futures_sequence {
 //    task"Реализуйте метод `fullSequence`" ()
     val seq=Future.sequence(futures)
     val result=Await.result(seq, scala.concurrent.duration.Duration.Inf)
-    val errors=futures.filter((f:Future[A])=>f.value.get.isFailure).map((f:Future[A])=>f.value.get.failed.get)
+    val errors=futures.filter((f:Future[A])=>f.value.get.isFailure).map((f:Future[A])=>f.value.get.toEither.left.get)
     println(result)
     println(errors)
     Future( (result  ,errors)  )
